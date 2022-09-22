1a4115089c25ceaed4d18b271c2580ff3bba18a2
b1906656 <ducb1906656@student.ctu.edu.vn>
2022-09-22 11:57:19 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
+
 
 const app = express();
 
@@ -11,7 +13,17 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ massage: "Welcome to contact book application." });
 });
-
 app.use("/api/contacts", contactsRouter);
+
+//handler 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resoure not found"));
+});
+//define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;

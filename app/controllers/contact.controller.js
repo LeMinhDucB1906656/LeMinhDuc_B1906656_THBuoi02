3386a1ed2bcc3ea3edc85123b85155d3d89283a1
b1906656 <ducb1906656@student.ctu.edu.vn>
2022-09-29 09:28:14 +0700

    Cai dat cac handler truy xuat CSDL

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -1,27 +1,106 @@
-exports.create = (req,res) => {
-    res.send({ massage: "create handler"});
-};
+const ContactService = require("../services/contact.service");
+const MongoDB = require("../utils/mongodb.util");
+const ApiError = require("../api-error");
 
-exports.findAll = (req,res) => {
-    res.send({ massage: "findAll handler"});
+exports.findAll = async (req, res, next) => {
+  let document = [];
+  try {
+    const contactService = new ContactService(MongoDB.client);
+    const { name } = req.query;
+    if (name) {
+      document = await contactService.findByName(name);
+    } else {
+      document = await contactService.find({});
+    }
+  } catch (error) {
+    return next(
+      new ApiError(500, "An error occurred while retrieving the contact")
+    );
+  }
+  return res.send(document);
 };
-
-exports.findOne = (req,res) => {
-    res.send({ massage: "findOne handler"});
+exports.findOne = async (req, res, next) => {
+  try {
+    const contactService = new ContactService(MongoDB.client);
+    const document = await contactService.findById(req.params.id);
+    if (!document) {
+      return next(new ApiError(404, "Contact not found"));
+    }
+    return res.send(document);
+  } catch (error) {
+    return next(
+      new ApiError(500, `Error retrieving contact with id=${req.params.id}`)
+    );
+  }
 };
-
-exports.update = (req,res) => {
-    res.send({ massage: "update handler"});
+exports.update = async (req, res, next) => {
+  if (Object.keys(req.body).length === 0) {
+    return next(new ApiError(400, "Data to update cannot be empty"));
+  }
+  try {
+    const contactService = new ContactService(MongoDB.client);
+    const document = await contactService.update(req.params.id, req.body);
+    if (!document) {
+      return next(new ApiError(404, "Contact not found"));
+    }
+    return res.send({ message: "Contact was update successfully" });
+  } catch (error) {
+    return next(
+      new ApiError(500, `Error updating contact with id=${req.params.id}`)
+    );
+  }
 };
-
-exports.delete = (req,res) => {
-    res.send({ massage: "delete handler"});
+exports.delete = async (req, res, next) => {
+  try {
+    const contactService = new ContactService(MongoDB.client);
+    const document = await contactService.delete(req.params.id);
+    if (!document) {
+      return next(new ApiError(404, "Contact not found"));
+    }
+    return res.send({ message: "Contact was deleted successfully" });
+  } catch (error) {
+    return next(
+      new ApiError(500, `Could not delete contact with id=${req.params.id}`)
+    );
+  }
 };
+exports.deleteAll = async (req, res, next) => {
+  try {
+    const contactService = new ContactService(MongoDB.client);
+    const deletedCount = await contactService.deleteAll();
 
-exports.deleteAll = (req,res) => {
-    res.send({ massage: "deleteAll handler"});
+    return res.send({
+      message: `${deletedCount} contacts were deleted successfully`,
+    });
+  } catch (error) {
+    return next(
+      new ApiError(500, "An error occurred while removing all contact")
+    );
+  }
 };
+exports.findAllFavorite = async (req, res, next) => {
+  try {
+    const contactService = new ContactService(MongoDB.client);
+    const document = await contactService.findFavorite();
 
-exports.findAllFavorite = (req,res) => {
-    res.send({ massage: "findAllFavorite handler"});
+    return res.send(document);
+  } catch (error) {
+    return next(
+      new ApiError(500, "An error occurred while retrieving favorite contact")
+    );
+  }
+};
+exports.create = async (req, res, next) => {
+  if (!req.body?.name) {
+    return next(new ApiError(400, "Name can notr be empty"));
+  }
+  try {
+    const contactService = new ContactService(MongoDB.client);
+    const document = await contactService.create(req.body);
+    return res.send(document);
+  } catch (error) {
+    return next(
+      new ApiError(500, "An error occurred while creating the contact")
+    );
+  }
 };
